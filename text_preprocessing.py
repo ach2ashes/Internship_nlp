62a17a34f4ae663b9ffd1960cd21de7797b221d2
ach2ashes <achraf.sbai2012@gmail.com>
2022-06-28 10:17:27 +0100

    added spacy preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,51 +1,97 @@
 #import libraries
 import nltk
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import WhitespaceTokenizer
+from nltk.tokenize import WhitespaceTokenizer , TreebankWordTokenizer,WordPunctTokenizer
 from nltk.corpus import stopwords
 import string
 import contractions
 from spellchecker import SpellChecker
 import re
 from french_lefff_lemmatizer.french_lefff_lemmatizer import FrenchLefffLemmatizer
+import unicodedata
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('words')
 nltk.download('wordnet')
-spell = SpellChecker()
 
 def remove_urls(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     return url_pattern.sub(r'', text)
 
+def remove_accented_chars(text):
+    new_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return new_text
 
-def text_preprocessing(text,lang):
+def text_preprocessing(text,lang,accented=True,tokenizer=WhitespaceTokenizer(),acccented=True,stopw=True,punctuation=True,lowercase=True,lemmatize=True,spelling=True,expand_contraction=True,urls=True):
     if lang.lower()=='english':
         stopword =stopwords.words('english')
         lemmatizer = WordNetLemmatizer()
+        spell = SpellChecker()
     else :#if lang="french"
         stopword = stopwords.words('french')
         lemmatizer = FrenchLefffLemmatizer()
-    #lowercase the text 
-    lower_text = text.lower()
-    #remove urls
-    lower_text=remove_urls(lower_text)
+        spell = SpellChecker(language="fr")
+    if lowercase:
+        #lowercase the text 
+        text = text.lower()
+    if urls:
+        #remove urls
+        text=remove_urls(text)
     #tokenize the text 
-    tokens = WhitespaceTokenizer().tokenize(lower_text)
-    #expand contractions
-    tokens_expanded = [contractions.fix(token) for token in tokens]
-    #remove punctuation
-    no_punct = [token for token in tokens_expanded if token not in string.punctuation]
-    #remove stopwords
-    no_stopwords = [token for token in no_punct if token not in stopword]
-    #spell check:
-    correct_words = [spell.correction(token) for token in no_stopwords]
-    #lemmatization : 
-    lemmatized = [lemmatizer.lemmatize(token) for token in correct_words]
-    return ' '.join(word for word in lemmatized)
+    tokens = tokenizer.tokenize(text)
+    if expand_contraction:
+        #expand contractions
+        tokens = [contractions.fix(token) for token in tokens]
+    if punctuation:
+        #remove punctuation
+        tokens = [token for token in tokens if token not in string.punctuation]
+    if stopw:
+        #remove stopwords
+        tokens = [token for token in tokens if token not in stopword]
+    if accented:
+        tokens = [remove_accented_chars(token) for token in tokens]
+    if spelling:
+        #spell check:
+        tokens = [spell.correction(token) for token in tokens]
+    if lemmatize:
+        #lemmatization : 
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return ' '.join(word for word in tokens)
 
     #Some tests:
-en_test = ' 13,000 people receive #wildfires evacuation orders in California'
-fr_test = 'plusieurs phrases pour un nettoyage'
-print(text_preprocessing(en_test,'english'))
-print(text_preprocessing(fr_test,'french'))
+file = 'C:/Users/PC2/Downloads/test-ex.txt'
+f=open(file,'r')
+data = f.read()
+print(text_preprocessing(data,lang='french'))
+
+#Now we ll do preprocessing using mainly spacy
+from spacy.lang.fr import French
+from spacy.lang.en import English
+import spacy
+#load only french and english models tokenizers
+nlp_en = spacy.load("en_core_web_sm", disable=['parser', 'tagger', 'ner'])
+nlp_fr = spacy.load("fr_core_news_sm", disable=['parser', 'tagger', 'ner'])
+
+def spacy_preprocessing(text,lang,lowercase=True,stopw=True,punctuation=True,alphabetic=True,lemmatize=True,):
+    if lang =="en":
+        nlp = nlp_en
+    else :
+        nlp = nlp_fr
+    if lowercase:
+        text = text.lower()
+    #tokenize with spacy's default tokenizer
+    tokens = nlp(text)
+    if stopw :
+        tokens = [token for token in tokens if not token.is_stop]
+    if lemmatize :
+        tokens = [token.lemma_.strip() for token in tokens]
+    if punctuation :
+        tokens = [re.sub('<[^>]*>', '', token) for token in tokens]
+    if alphabetic:
+        tokens = [re.sub('[\W]+','',token.lower()) for token in tokens]
+    return ' '.join(word for word in tokens)
+
+file = 'C:/Users/PC2/Downloads/test-ex.txt'
+f=open(file,'r')
+data = f.read()
+print(spacy_preprocessing(data,lang='french'))
