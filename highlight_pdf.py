80d3296bcd72281698148b36e753c5db5a643e16
ach2ashes <achraf.sbai2012@gmail.com>
2022-07-18 01:39:17 +0100

    added highlighting pdf api

--- highlight_pdf.py
+++ highlight_pdf.py
@@ -1,6 +1,7 @@
 # Import Libraries
 import fitz
-from text_preprocessing import ner_spacy
+from ner import ner_spacy , find_imo,find_swift
+import base64
 #extract text from pdf
 def extract_text(input_file):
     doc = fitz.open(input_file)
@@ -11,11 +12,17 @@ def extract_text(input_file):
 
 #find entities
 def find_ent(input_file):
-    text_ents  = [ner_spacy(txt)[0] for txt in  extract_text(input_file)] #ner_spacy(txt)[0]:entities
+    text_ents  =[]
+    for txt in  extract_text(input_file):
+        page_ents = []
+        page_ents.extend(ner_spacy(txt)[0]) #ner_spacy(txt)[0]:entities
+        page_ents.extend(find_imo(txt))
+        page_ents.extend(find_swift(txt))
+        text_ents.append(page_ents)
     return text_ents
 
 #highlight entities in pages
-def highlight_ent(page , matching_ents,color_map):
+def highlight_ent(page , matching_ents):
     for ent in matching_ents:
         matching_val_area = page.search_for(ent.text)
         highlight = page.addHighlightAnnot(matching_val_area)
@@ -25,16 +32,23 @@ def highlight_ent(page , matching_ents,color_map):
         highlight.set_info(info)
         highlight.update()
     return "highliting done"
+#pdf to base64
+def pdf_to_base64(pdf):
+    with open(pdf, "rb") as pdf_file:
+        encoded_string = base64.b64encode(pdf_file.read())
+    return encoded_string
+#base64 to pdf
+def base64_to_pdf(base64):
+    with open('out.pdf', 'wb') as pdf:
+        pdf.write(base64.b64decode(base64))
+    return ".//out.pdf"
 #final function
 def output(input_file):
     doc=fitz.open(input_file)
-    text = extract_text(input_file)
     text_ents = find_ent(input_file)
-    labels = ner_spacy(text[0])[1]
-    color_map = dict(zip(labels, list_of_colors[:len(labels)]))
     i=0
     for page in doc:
-        highlight_ent(page,text_ents[i],color_map)
+        highlight_ent(page,text_ents[i])
         i+=1
     doc.save("output.pdf", garbage=4, deflate=True, clean=True)
-    return "updated pdf"
+    return text_ents,".//ouput.pdf"
--- text_preprocessing.py
+++ text_preprocessing.py
@@ -102,78 +102,8 @@ f=open(file,'r')
 data = f.read()
 #print(spacy_preprocessing(data,lowercase=False))
 
-#Definig NER for nltk and spacy
 
 
-import spacy
-
-
-def ner_nltk(text):
-    assert detect(text) =='en' 'text should be english to be parsed with nltk'
-    tokens = word_tokenize(text)
-    tagged_tokens  = nltk.pos_tag(tokens)
-    chunked = nltk.ne_chunk(tagged_tokens)
-    for chunk in chunked : 
-        if hasattr(chunk,"label") and chunk.label == "NE":
-            print(chunk)
-
-
-def ner_spacy(text):
-    if detect(text) == "en":
-        ner  = spacy.load("en_core_web_sm",disable=["tagger","parser"])
-    else:
-        ner  = spacy.load("fr_core_news_sm",disable=["tagger","parser"])
-    labels = ner.get_pipe("ner").labels
-    return ner(text).ents,labels
-
-
-
-#Comparing results before\after preprocessing
-
-#Testing preprocessing filters
-from itertools import chain , combinations
-def powerset(iterable): # return all possible subsets of iterable
-    s = list(iterable)
-    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
-
-list_of_params=["accented","stopw","punctuation","lowercase","lemmatize","spelling","expand_contraction","urls"] 
-dict_of_params=dict.fromkeys(list_of_params,[True])
-
-def filter_combinations(params):#return all the possible filter combinations, param; list of parametrs with a default value
-    list_of_combs = []
-    combs=powerset([i for i in range(len(params)) ])
-    for comb in combs:
-        initial_conf = [True for i in range(len(params))]
-        if len(comb)==0 :
-            list_of_combs.append(initial_conf)
-        else:
-            for i in comb:
-                initial_conf[i]=False
-            list_of_combs.append(initial_conf)
-    return list_of_combs
-
-import pandas as pd 
-pd.set_option('display.max_columns', 100)
-pd.set_option('display.max_rows', 500)
-columns = []
-columns.extend(list_of_params)
-ff=filter_combinations(list_of_params)#all possible filter combinations
-def results(text,preprocessing_function):
-    table=[]
-    labels = ner_spacy(text)[1]
-    columns.extend(labels)
-    for comb in ff:#comb is a combinations of active filters 
-        preprocessed_text=preprocessing_function(text,*comb)
-        entities = ner_spacy(preprocessed_text)[0]
-        dictionn = dict.fromkeys(labels,0)
-        for ent in entities:
-            for key in dictionn.keys():
-                if ent.label_==key:
-                    dictionn[key]+=1
-        comb.extend(dictionn.values())
-        table.append(comb)       
-    return pd.DataFrame(table,columns=columns)
-#french NER in spaCy has only  4 labels
 
 
 
